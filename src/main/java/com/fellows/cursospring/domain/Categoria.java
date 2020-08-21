0f54303284312081b22d7b7d25ccfba990d88811
Andre Fellows <fellows.devel@gmail.com>
2020-08-21 20:23:50 -0300

    adicionado banco de dados h2 com criaçao automatica das tabelas a partir dos objetos

--- src/main/java/com/fellows/cursospring/domain/Categoria.java
+++ src/main/java/com/fellows/cursospring/domain/Categoria.java
@@ -2,12 +2,20 @@ package com.fellows.cursospring.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 6559260849601963581L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
 	public Categoria() {
 	}
 
@@ -63,5 +71,4 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-	
 }
