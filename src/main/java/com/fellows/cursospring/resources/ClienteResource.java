c2c7dcfcc99fe4225655777b8e69ec3b1d68c843
Andre Fellows <fellows.devel@gmail.com>
2020-08-23 22:02:46 -0300

    adicionado cliente serice e resource

---
+++ src/main/java/com/fellows/cursospring/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.fellows.cursospring.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fellows.cursospring.domain.Cliente;
+import com.fellows.cursospring.services.ClienteService;
+import com.fellows.cursospring.services.exception.DataNotFoundException;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws DataNotFoundException {
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/fellows/cursospring/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.fellows.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fellows.cursospring.domain.Cliente;
+import com.fellows.cursospring.repositories.ClienteRepository;
+import com.fellows.cursospring.services.exception.DataNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) throws DataNotFoundException {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new DataNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
